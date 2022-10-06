ffdceb5f9b23b0a62e6cd41c96f05c2b3f5fd522
Davidson Pierre <2094516@cegeplimoilou.ca>
2022-10-06 14:16:54 -0400

    Creation des test Unitaires pour les classes De6(), Piece(), Face() qui fonctionnent

--- TestDeWPF/UnitTest1.cs
+++ TestDeWPF/UnitTest1.cs
@@ -3,11 +3,40 @@ using DeWPF;
 namespace TestDeWPF
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestDeWPF
     {
+        // Test du constructeur de la classe Face()
         [TestMethod]
-        public void TestMethod1()
+        public void TestFace()
         {
+            Face nbFace = new (5, "Pentagonal");
+            Face septFace = new (7, "Septagonal");
+
+            Assert.AreEqual(nbFace.Nom, "Pentagonal");
+            Assert.AreEqual(septFace.Valeur, 7);
+            Assert.AreEqual(nbFace.Valeur, 5);
+            Assert.AreEqual(septFace.Nom, "Septagonal");
+            Assert.AreNotSame(septFace.Valeur, 5);
         }
+
+        // test de la classe Piece()
+        [TestMethod]
+        public void TestPiece()
+        {
+            Piece piece = new Piece();
+            Assert.AreEqual(piece.NbFaces, 2);
+            Assert.AreEqual(piece.Nom, "Pièce");
+            Assert.AreEqual(piece.Faces[0].Nom, "pile");
+        }
+
+        // Test d la lasse De6
+        [TestMethod]
+        public void TestD6()
+        {
+            De6 de = new De6();
+            Assert.AreEqual(de.NbFaces, 6);
+            Assert.AreEqual(de.Nom, "Dé 6");
+        }
+
     }
 }
